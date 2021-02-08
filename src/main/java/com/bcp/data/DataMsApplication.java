26c2dd348e91dcc1a5ba8643b054006816ac81aa
Leonardo Vargas <leonardo.vargas@la-mark.com>
2021-02-08 16:08:58 -0500

    first commit

---
+++ src/main/java/com/bcp/data/DataMsApplication.java
@@ -0,0 +1,13 @@
+package com.bcp.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataMsApplication.class, args);
+    }
+
+}
